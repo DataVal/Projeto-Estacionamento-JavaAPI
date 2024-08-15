4e39a13be1a9a3480cc435ddefce0ee9931a255c
DataVal <reggentia@gmail.com>
2024-08-14 21:22:45 -0300

    Criado o recurso de atualizar senha :)

--- src/main/java/com/valerio/demo_park_api/web/controller/UsuarioController.java
+++ src/main/java/com/valerio/demo_park_api/web/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package com.valerio.demo_park_api.web.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,6 +32,16 @@ public class UsuarioController {
     //Criando a funcionalidade do GET
     public ResponseEntity<Usuario> getById(@PathVariable Long id) {
         Usuario user = usuarioService.buscarPorId(id);
+        return ResponseEntity.ok(user);
+    }
+
+    @PatchMapping("/{id}")
+    //PutMapping x PatchMapping ? Basicamente o Put faz uma atualização total em um objeto
+    //Como queremos alterar apenas a propriedade SENHA usaremos nesse caso o Patch
+    //Criando a funcionalidade semelhante ao do GET pois ele irá usar o id para identificar o user a ter a senha alterada
+    public ResponseEntity<Usuario> updatePassword(@PathVariable Long id, @RequestBody Usuario usuario) {
+        //Uso do parâmetro "@RequestBody Usuario usuario" pois a senha irá no corpo da requisição ao invés da url
+        Usuario user = usuarioService.editarSenha(id, usuario.getPassword());
         return ResponseEntity.ok(user);
     }
 }

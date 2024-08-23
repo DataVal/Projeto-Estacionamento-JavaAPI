6ce2f26adcf0af22b7f495aa32ef83c2eb3a452f
DataVal <reggentia@gmail.com>
2024-08-22 21:33:46 -0300

    Validação de senha funcionando com Jakarta e DTO

--- src/main/java/com/valerio/demo_park_api/web/dto/UsuarioSenhaDto.java
+++ src/main/java/com/valerio/demo_park_api/web/dto/UsuarioSenhaDto.java
@@ -1,4 +1,6 @@
 package com.valerio.demo_park_api.web.dto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,8 +10,14 @@ import lombok.ToString;
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
 public class UsuarioSenhaDto {
 
+    @NotBlank
+    @Size(min = 6,max = 6) 
     private String senhaAtual;
+    @NotBlank
+    @Size(min = 6,max = 6) 
     private String novaSenha;
+    @NotBlank
+    @Size(min = 6,max = 6) 
     private String confirmaSenha;
 
 }
